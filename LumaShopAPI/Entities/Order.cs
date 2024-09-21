109c4b68a2117a544b196b232208021711dc4f9d
Imash Nipun <imash@ensiz.com>
2024-09-22 04:53:28 +0530

    Implement crud operations for order functionalities.

---
+++ LumaShopAPI/Entities/Order.cs
@@ -0,0 +1,63 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using LumaShopAPI.LumaShopEnum;
+
+namespace LumaShopAPI.Entities
+{
+    public class Order
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("id")]
+        public string Id { get; set; }
+
+        [BsonElement("customerId")]
+        [BsonRequired]
+        public string CustomerId { get; set; }
+
+        [BsonElement("items")]
+        [BsonRequired]
+        public List<Item> Items { get; set; }
+
+        [BsonElement("totalAmount")]
+        [BsonRequired]
+        public decimal TotalAmount { get; set; }
+
+        [BsonElement("status")]
+        [BsonRequired]
+        public OrderStatusEnum Status { get; set; } = OrderStatusEnum.PENDING;
+
+        [BsonElement("createdBy")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CreatedBy { get; set; }
+
+        [BsonElement("createdAt")]
+        [BsonDateTimeOptions]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [BsonElement("updatedAt")]
+        [BsonDateTimeOptions]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        [BsonElement("isArchive")]
+        [BsonDefaultValue(false)]
+        public bool IsArchive { get; set; } = false;
+
+    }
+
+    public class Item
+    {
+        [BsonRequired]
+        [BsonElement("productId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductId { get; set; }
+
+        [BsonRequired]
+        [BsonElement("quantity")]
+        public int Quantity { get; set; }
+
+        [BsonRequired]
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+    }
+}
