55597914a70c0a7184bde7a705f0fa94197cceab
Imash Nipun <imash@ensiz.com>
2024-09-22 06:44:36 +0530

    Implement crud operation for vendor rating functionalities.

---
+++ LumaShopAPI/Entities/VendorRating.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+namespace LumaShopAPI.Entities
+{
+
+    public class VendorRatings
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("vendorId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string VendorId { get; set; }
+
+        [BsonElement("customerId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CustomerId { get; set; }
+
+        [BsonElement("rating")]
+        public int? Rating { get; set; }
+
+        [BsonElement("comment")]
+        public string Comment { get; set; } = string.Empty;
+
+        [BsonElement("createdAt")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [BsonElement("updatedAt")]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
